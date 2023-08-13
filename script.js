b9d716abd5be8a35c1bba1c5bdd1768960b96a3d
dagguapti vinaykumar <112471376+vinaykumardag1@users.noreply.github.com>
2023-08-13 13:23:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function iqoo(){
+    open("https://www.amazon.in/s?k=iqoo+neo+7+pro&sprefix=Iqoo%2Caps%2C418&ref=nb_sb_ss_ts-doa-p_1_4");
+}
+function oneplus(){
+    open("https://www.amazon.in/OnePlus-Galactic-Silver-256GB-Storage/dp/B0BSNNYHY5/ref=sr_1_3?keywords=oneplus+11r&qid=1691643046&sprefix=ONep%2Caps%2C817&sr=8-3");
+}
+function nothing(){
+    open("https://www.flipkart.com/search?q=nothing+phone+2&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_2_7_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_2_7_na_na_ps&as-pos=2&as-type=RECENT&suggestionId=nothing+phone+2%7CMobiles&requestId=367a3348-a137-487d-9aef-ae473dfba9a8&as-backfill=on");
+}
+function oneplus11(){
+    open("https://www.amazon.in/s?k=oneplus+11&sprefix=Onep%2Caps%2C611&ref=nb_sb_ss_ts-doa-p_6_4");
+}
+function S23ultra(){
+    open("https://www.amazon.in/s?k=s22+ultra+samsung&sprefix=S22+%2Caps%2C315&ref=nb_sb_ss_ts-doa-p_2_4");
+}
+function Edge40(){
+    open("https://www.flipkart.com/search?q=motorola+edge+40&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_2_4_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_2_4_na_na_ps&as-pos=2&as-type=RECENT&suggestionId=motorola+edge+40%7CMobiles&requestId=14733dad-7a56-4f98-a63d-8d72e953fe74&as-backfill=on");
+}
+function ZFold5(){
+    open("https://www.amazon.in/s?k=z+fold+5&sprefix=ZFOl%2Caps%2C403&ref=nb_sb_ss_ts-doa-p_1_4");
+}
+function ZFlip5(){
+    open("https://www.amazon.in/s?k=zllip+5&crid=3J7FE5S65TTEW&sprefix=zllip%2Caps%2C295&ref=nb_sb_noss");
+}
+function razr40ultra(){
+    open("https://www.amazon.in/s?k=motorola+razr+40+ultra&sprefix=razr%2Caps%2C461&ref=nb_sb_ss_ts-doa-p_1_4");
+}
+function click(){
+    open("https://www.youtube.com/@Prasadtechintelugu");
+}
